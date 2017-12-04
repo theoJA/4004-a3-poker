a250c156830f699c6d6409b095360908c2ecaa44
theoJA <theophilus.ja@gmail.com>
2017-12-03 21:20:31 -0500

    Added socket stuff

--- socket-io-server/app.js
+++ socket-io-server/app.js
@@ -12,20 +12,55 @@ app.use(index);
 const server = http.createServer(app);
 const io = socketIo(server);
 
+// -----------------------------------------------------------------
+
+const GameSession = require('./models/GameSession');
+const Player = require('./models/Player');
+
+const numPlayers = null;
+const players = [];
+
+const maxPlayers = 4;
+
+// --------------- ON CONNECTION -----------------------------------------
+
 io.on("connection", socket => {
-  console.log("New client connected"), setInterval(
-    () => getApiAndEmit(socket), 10000
-  );
-  socket.on("disconnect", () => console.log("Client disconnected"));
+
+  console.log("New player connected");
+
+  if (players.length === 0) {
+    welcomePlayerOne(socket);
+    let player = new Player(players.length, "human", 0);
+    players.push(player);
+  }
+  else if (players.length > 0 && players.length < 4 ) {
+    welcomeOtherPlayers(socket);
+    let player = new Player(players.length, "human", 0);
+    players.push(player);
+  }
+  else {
+    maxPlayersMessage(socket);
+  }
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+    players.pop();
+  });
 });
 
-const getApiAndEmit = async socket => {
-  try {
-    const res = await axios.get("https://api.darksky.net/forecast/4018ea08544401960664dfa69dc55339/43.7695,11.2558");
-    socket.emit("FromAPI", res.data.currently.temperature);
-  } catch (error) {
-    console.error(`Error: ${error.code}`);
-  }
+// ---------------- EMIT FUNCTIONS -----------------------------------
+const welcomePlayerOne = socket => {
+  socket.emit("PlayerMessage", 'WELCOME, PLAYER 1!');
 };
 
+const welcomeOtherPlayers = socket => {
+  socket.emit("PlayerMessage", `WELCOME, OTHER PLAYER!`);
+};
+
+const maxPlayersMessage = socket => {
+  socket.emit("PlayerMessage", 'UNABLE TO JOIN! MAX PLAYERS REACHED! ☹');
+} 
+
+
+// ------------------------------------------------------------------------
 server.listen(port, () => console.log(`Listening on port ${port}`));
