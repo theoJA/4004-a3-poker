36706da30e31380eefc5aea8c92b77ad03e9015a
theoJA <theophilus.ja@gmail.com>
2017-12-04 22:05:58 -0500

    emit: gameOptions, on: humansJoined (not working though)

--- socket-io-client/src/components/API.js
+++ socket-io-client/src/components/API.js
@@ -6,6 +6,20 @@ function getPlayerMessage(cb) {
   socket.on("PlayerMessage", data => cb(data));
 };
 
+function getHumanPlayerJoined(cb) {
+  console.log('receiving data');
+  socket.on("HumanPlayerJoined", data => {
+    console.log(data);
+    cb(data)});
+  console.log('received data');
+}
+
+function postGameOptions({opponent, numOpponents}) {
+  socket.emit("GameOptions", {opponent, numOpponents});
+}
+
 export {
   getPlayerMessage,
+  getHumanPlayerJoined,
+  postGameOptions,
 };
