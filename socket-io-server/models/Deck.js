bdc944d3f6ee48b2bb565e197b1c4cf04842ca17
theoJA <theophilus.ja@gmail.com>
2017-12-07 01:33:19 -0500

    initDeck, initHands, and rearrangeHands in ascending order

--- socket-io-server/models/Deck.js
+++ socket-io-server/models/Deck.js
@@ -1,25 +1,77 @@
-import Card from "./Card";
+const Card = require("./Card");
 
-class Deck {
+module.exports = class Deck {
 
   constructor() {
-    this.suites = ['spades', 'clubs', 'hearts', 'diamonds'];
-    this.ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
-    this.faces = ['faceup', 'facedown'];
+    this.suites = ['s', 'c', 'h', 'd'];
+    this.ranks = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'];
+    this.rankValues = ['2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14'];
+    this.faces = ['facedown', 'faceup'];
     this.deck = [];
+    this.handArr = ['firstHand', 'secondHand', 'thirdHand', 'fourthHand'];
     this.hands = {
       firstHand: [],
       secondHand: [],
       thirdHand: [],
       fourthHand: [],
     };
+
+    this.initDeck();
+    this.initHands();
+    this.rearrangeHandsAscending();
   }
 
-  _initDeck() {
+  initDeck() {
+    let { suites, ranks, faces, deck, handArr, hands, rankValues } = this;
+
+    for (let i = 0; i < suites.length; i++) {
+      for (let j = 0; j < ranks.length; j++) {
+        deck.push(new Card(suites[i], ranks[j], ranks[j] + suites[i], faces[0], rankValues[j]));
+      }
+    }
+  }
+
+  initHands() {
+    let { suites, ranks, faces, deck, handArr, hands } = this;
+    let tempCard, randIndex, tempDeck; 
+    
+    for(let i = 0; i < 5; i++) {
+      handArr.forEach(hand => {
+        
+        randIndex = Math.floor(Math.random(0,deck.length) * deck.length);
+        tempCard = deck.splice(randIndex,1);
+        hands[hand].push(tempCard[0]);
+      });
+    }
+  }
+
+  rearrangeHandsAscending() {
+    let { suites, ranks, faces, deck, handArr, hands } = this;
+    let minIdx, temp;
+
+    handArr.forEach(hand => { 
+      for (let i = 0; i < hands[hand].length; i++) {
+        minIdx = i;
+        for (let j = i+1; j < hands[hand].length; j++) {
+          if (parseInt(hands[hand][j]._getNumValue()) < parseInt(hands[hand][minIdx]._getNumValue()))
+            minIdx = j;
+        }
+        temp = hands[hand][i];
+        hands[hand][i] = hands[hand][minIdx];
+        hands[hand][minIdx] = temp;
+      }      
+    });
+  }
   
+  getDeck() {
+    return this.deck;
+  }
+  
+  getHands() {
+    return this.hands;
   }
 
-  _initHands() {
-
+  getHandArr() {
+    return this.handArr;
   }
 }
